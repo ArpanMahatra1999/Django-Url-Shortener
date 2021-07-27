e70c573db69e0546e5179fd23ed95f9b01d27ee5
unknown <arpanmahatra@prixatech.com>
2021-07-27 14:41:46 +0545

    Static root setup

--- config/settings.py
+++ config/settings.py
@@ -119,10 +119,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR, 'static'
 ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DISABLE_COLLECTSTATIC=1
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
